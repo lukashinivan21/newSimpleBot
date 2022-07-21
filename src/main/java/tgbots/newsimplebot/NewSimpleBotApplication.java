716a29f9d6a37279fc7fd6fe645057a2699dec22
Ivan <uvanes21@yandex.ru>
2022-07-21 11:01:41 +0300

    Initial commit

---
+++ src/main/java/tgbots/newsimplebot/NewSimpleBotApplication.java
@@ -0,0 +1,13 @@
+package tgbots.newsimplebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSimpleBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewSimpleBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tgbots/newsimplebot/NewSimpleBotApplicationTests.java
@@ -0,0 +1,13 @@
+package tgbots.newsimplebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSimpleBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
